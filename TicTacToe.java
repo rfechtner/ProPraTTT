ee657d7cbcb12aae9e580609d3a519b1dd713b28
rfechtner <ronfechtner@gmail.com>
2015-10-22 14:07:48 +0200

    Compiled Setup: ready to go

---
+++ TicTacToe.java
@@ -0,0 +1,11 @@
+public class TicTacToe extends basicpackage.BasicBoard implements Game {
+
+  public static void main(String[] args){
+    System.out.println("Test");
+  }
+
+  @Override
+  public String gameStatus(){
+    return "";
+  }
+}
